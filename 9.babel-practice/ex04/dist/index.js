7b5c9bbad398908d30305dffe3b3341974aca17d
WooGeun-Nam <rolru7686@gmail.com>
2023-03-08 09:52:34 +0900

    babel #finish

---
+++ 9.babel-practice/ex04/dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// 블록 스코프
+const users = [{
+  no: 1,
+  name: '마이콜',
+  email: 'michol@gmail.com'
+}, {
+  no: 2,
+  name: '둘리',
+  email: 'dooly@gmail.com'
+}];
+
+// 객체 분해
+
+function print(_ref) {
+  let {
+    no,
+    name,
+    email
+  } = _ref;
+  // 템플릿 문자열(es6)
+  console.log(`${no}:${name}:${email}`);
+}
+
+// 템플릿 문자열
+// for..of(es6)
+for (let user of users) {
+  print(user[0]);
+}
