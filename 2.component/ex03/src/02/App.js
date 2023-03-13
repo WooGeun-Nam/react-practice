ce9b05c102845399dd29a8371c4b18c743ddbde1
WooGeun-Nam <rolru7686@gmail.com>
2023-03-13 11:26:52 +0900

    event #2

--- 2.component/ex03/src/02/App.js
+++ 2.component/ex03/src/02/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TitleBar01 from './TitleBar01';
+import TitleBar02 from './TitleBar02';
 
 export default function () {
     return (
         <div>
             <TitleBar01/>
+            <TitleBar02/>
         </div>
     );
 }
